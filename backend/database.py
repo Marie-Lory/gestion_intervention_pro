0b70cabc92c10094a84a39374caeaa508f72997e
Marie-Lory <lory.marie02@outlook.fr>
2026-05-11 13:19:40 -0400

    modif

--- backend/database.py
+++ backend/database.py
@@ -11,7 +11,7 @@ engine = create_engine(
     DATABASE_URL,
     connect_args={
         "ssl": {
-            "ca": "/etc/ssl/certs/ca-certificates.crt"
+            "ssl-mode": "REQUIRED"
         }
     }
 )
