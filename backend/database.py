1a4ff6d216d764064aca381f9db650fdd5cbd9c5
Marie-Lory <lory.marie02@outlook.fr>
2026-05-11 15:00:53 -0400

    modif

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,33 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+SSL_CA = os.path.join(
+    BASE_DIR,
+    "certs",
+    "ca.pem"
+)
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={
+        "ssl": {
+            "ca": SSL_CA
+        }
+    }
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
 
 Base = declarative_base()
